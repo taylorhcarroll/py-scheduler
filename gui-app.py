3cb247e9f5f53ab1035a34e5b1b26087de6ad940
taylorhcarroll <taylorhcarroll1@gmail.com>
2022-05-01 22:15:24 -0500

    gui app

--- gui-app.py
+++ gui-app.py
@@ -4,6 +4,7 @@ import os
 
 # this will hold whole app
 root = tk.TK()
+apps = []
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
@@ -13,8 +14,22 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 # specify initial directory
 def addApp():
+    # widget refers to all the labels in frame
+    # destro clears out old stuff
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename= filedialog.askopenfilename(initialdir="/", title="Select File",
                                                 filetypes=(("exectuables", "*exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 # add button
 # can also add to frame but in this case attach to root
@@ -25,7 +40,8 @@ openFile = tk.Button(root, text="Open File", padx=10,
 # attaches button 
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, 
+                    fg="white", bg="#263D42", command="runApps")
 runApps.pack()
 
 
