e0280f570f5e35d05829fbca51e10a5df18b14a8
taylorhcarroll <taylorhcarroll1@gmail.com>
2022-05-08 15:16:53 -0500

    added list of common examples to execute

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,26 @@
 import schedule
+import time
 
 def job():
-    print("A simple Python scheduler")
+    print("I'm working...")
 
-# do() specifies the job_func that should be called every time the job runs
-schedule.every(2).seconds.do(job)
+def job2():
+    print("yo boiss..")
+
+def job3():
+    print("Hello")
+
+schedule.every(5).seconds.do(job)
+# some other variations 
+schedule.every().hour.do(job)
+# by default, the schedule module uses the 24hr format
+schedule.every().day.at("12:25").do(job)
+schedule.every(5).to(10).minutes.do(job)
+schedule.every().thursday.at("19:15").do(job)
+schedule.every().wednesday.at("13:15").do(job)
+schedule.every().minute.at(":17").do(job)
+schedule.every(2).seconds.do(job2)
 
 while True:
     schedule.run_pending()
+    time.sleep(1)
